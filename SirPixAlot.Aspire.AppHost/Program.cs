ec029968aa405c47d4670ebd8c2d924fd39a02a6
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-09 04:04:44 +0800

    update container stuff

--- SirPixAlot.Aspire.AppHost/Program.cs
+++ SirPixAlot.Aspire.AppHost/Program.cs
@@ -1,4 +1,4 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.SirPixAlot_WebAPI>("WebAPI");
+builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
 builder.Build().Run();
