b7d049a577066ef557cf4196674dad20249b3c9f
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-09 20:55:49 +0800

    added ingress

--- SirPixAlot.Aspire.AppHost/Program.cs
+++ SirPixAlot.Aspire.AppHost/Program.cs
@@ -1,4 +1,10 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
+var sirPixAlotWebAPI =  builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
+if (builder.Environment.EnvironmentName == "Production")
+{
+    sirPixAlotWebAPI.WithEnvironment("EventStoreConfig__SirPixAlotGrainStorageDev", "SirPixAlotGrainStorage");
+}
 builder.Build().Run();
