0ffa1e0f7fa20f3d6b43731dbd8e0d2209176be8
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-10 17:10:27 +0800

    added secret and aspire state

--- SirPixAlot.Aspire.AppHost/Program.cs
+++ SirPixAlot.Aspire.AppHost/Program.cs
@@ -3,10 +3,12 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var grainStorage = builder.AddParameter("EventStoreConfig-SirPixAlotGrainStorage");
-var testSecret = builder.AddParameter("testSecret",secret: true);
+var awsAccessKeyId = builder.AddParameter("AwsAccessKeyId", secret: true);
+var awsSecretAccessKey = builder.AddParameter("AwsSecretAccessKey", secret: true);
 
 var sirPixAlotWebAPI =  builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
 sirPixAlotWebAPI.WithEnvironment("EventStoreConfig__SirPixAlotGrainStorage", grainStorage);
-sirPixAlotWebAPI.WithEnvironment("testSecret", testSecret);
+sirPixAlotWebAPI.WithEnvironment("AWS_ACCESS_KEY_ID", awsAccessKeyId);
+sirPixAlotWebAPI.WithEnvironment("AWS_SECRET_ACCESS_KEY", awsSecretAccessKey);
 
 builder.Build().Run();
