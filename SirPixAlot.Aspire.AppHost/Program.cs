e25f238482edaa15fccfe7d3ed59fa40d0065152
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-11 09:10:08 +0800

    added aws region

--- SirPixAlot.Aspire.AppHost/Program.cs
+++ SirPixAlot.Aspire.AppHost/Program.cs
@@ -3,10 +3,12 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var grainStorage = builder.AddParameter("EventStoreConfig-SirPixAlotGrainStorage");
+var awsRegion = builder.AddParameter("AwsRegion");
 var placeHolder = builder.AddConnectionString("ConnectionString-PlaceHolder", "PlaceHolder");
 
 var sirPixAlotWebAPI =  builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
 sirPixAlotWebAPI.WithEnvironment("EventStoreConfig__SirPixAlotGrainStorage", grainStorage);
 sirPixAlotWebAPI.WithEnvironment("ConnectionString__PlaceHolder", placeHolder);
+sirPixAlotWebAPI.WithEnvironment("AWS_REGION", awsRegion);
 
 builder.Build().Run();
