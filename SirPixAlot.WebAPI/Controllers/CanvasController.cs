dd6a796ee5b1d54a9d146934945c208536d7f69b
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-20 17:16:38 +0800

    added rectangle endpoint

--- SirPixAlot.WebAPI/Controllers/CanvasController.cs
+++ SirPixAlot.WebAPI/Controllers/CanvasController.cs
@@ -13,12 +13,36 @@ namespace SirPixAlot.WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> Get(long x, long y)
         {
+            var dto = await GetPixelDto(grainFactory, x, y);
+            return Ok(dto);
+        }
+
+        private static async Task<PixelDto> GetPixelDto(IGrainFactory grainFactory, long x, long y)
+        {
             var pixel = grainFactory.GetGrain<IPixelGrain>(x, y.ToString());
-            return Ok(new PixelDto { 
+            return new PixelDto
+            {
                 X = x,
                 Y = y,
                 Color = await pixel.GetColor()
-            });
+            };
+        }
+
+        [HttpGet("rectangle")]
+        public async Task<IActionResult> Get(long topLeftX, long topLeftY, long bottomRightX, long bottomRightY)
+        {
+            //var pixel = grainFactory.GetGrain<IPixelGrain>(x, y.ToString());
+            var pixelTasks = new List<Task<PixelDto>>();
+            for (var x = topLeftX; x <= bottomRightX; x++) 
+            { 
+                for (var y = bottomRightY; y <= topLeftY; y++)
+                {
+                    var task = GetPixelDto(grainFactory, x, y);
+                    pixelTasks.Add(task);
+                }
+            }
+            await Task.WhenAll(pixelTasks);
+            return Ok(pixelTasks.Select(t => t.Result));
         }
 
         [HttpPut]
