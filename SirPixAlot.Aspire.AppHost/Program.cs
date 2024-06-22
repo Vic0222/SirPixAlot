ffa5a9df2e30d84afacb06ec9cfcce9e40b945e9
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-22 16:05:23 +0800

    added new env var

--- SirPixAlot.Aspire.AppHost/Program.cs
+++ SirPixAlot.Aspire.AppHost/Program.cs
@@ -2,13 +2,15 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var grainStorage = builder.AddParameter("EventStoreConfig-SirPixAlotGrainStorage");
+var grainStorage = builder.AddParameter("EventStoreConfig-SirPixAlotGrainStorage"); 
 var awsRegion = builder.AddParameter("AwsRegion");
-var placeHolder = builder.AddConnectionString("ConnectionString-PlaceHolder", "PlaceHolder");
+var libsqlReplicaPath = builder.AddParameter("LibsqlDatabaseClientOptions-ReplicaPath");
+var secretsPlaceHolder = builder.AddConnectionString("ConnectionString-PlaceHolder", "PlaceHolder");
 
 var sirPixAlotWebAPI =  builder.AddProject<Projects.SirPixAlot_WebAPI>("SirPixAlotWebAPI");
 sirPixAlotWebAPI.WithEnvironment("EventStoreConfig__Table", grainStorage);
-sirPixAlotWebAPI.WithEnvironment("ConnectionString__PlaceHolder", placeHolder);
+sirPixAlotWebAPI.WithEnvironment("ConnectionString__PlaceHolder", secretsPlaceHolder);
 sirPixAlotWebAPI.WithEnvironment("AWS_REGION", awsRegion);
+sirPixAlotWebAPI.WithEnvironment("LibsqlDatabaseClientOptions__ReplicaPath", libsqlReplicaPath);
 
 builder.Build().Run();
