d7d53cc5803d17c2e8bf1b08508c479826a85ff3
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-23 20:32:40 +0800

    added performanceTest end point

--- SirPixAlot.WebAPI/Controllers/CanvasController.cs
+++ SirPixAlot.WebAPI/Controllers/CanvasController.cs
@@ -10,6 +10,7 @@ namespace SirPixAlot.WebAPI.Controllers
     [ApiController]
     public class CanvasController(IGrainFactory grainFactory) : ControllerBase
     {
+
         [HttpGet]
         public async Task<IActionResult> Get(long x, long y)
         {
@@ -45,6 +46,33 @@ namespace SirPixAlot.WebAPI.Controllers
             }
 
             return Ok(pixelDtos);
+        }
+
+        [HttpGet("rectanglePerfTest")]
+        public async Task<IActionResult> GetRectanglePerfTest(long topLeftX, long topLeftY, long bottomRightX, long bottomRightY)
+        {
+            //We originally tried to get pxels at the same time here and used Task.WhenAll.
+            //But that seems to hammer the silo's too hard.
+            //So we do await here one by one to lessen load.
+
+            const int batchSize = 100;
+            var pixelDtos = new List<Task<PixelDto>>();
+            for (var x = topLeftX; x <= bottomRightX; x++)
+            {
+                for (var y = bottomRightY; y <= topLeftY; y++)
+                {
+                    var pixelDto = GetPixelDto(grainFactory, x, y);
+                    pixelDtos.Add(pixelDto);
+                    if (x + y % batchSize == 0)
+                    {
+                        await Task.WhenAll(pixelDtos);
+                    }
+                }
+            }
+
+            await Task.WhenAll(pixelDtos);
+
+            return Ok(pixelDtos.Select(p => p.Result));
         }
 
         [HttpPut]
