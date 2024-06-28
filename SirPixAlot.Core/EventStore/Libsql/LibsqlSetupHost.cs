9aacb6a5b62d81bbf951f227daad33966a1a3597
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-06-28 23:02:56 +0800

    only sync on start up

--- SirPixAlot.Core/EventStore/Libsql/LibsqlSetupHost.cs
+++ SirPixAlot.Core/EventStore/Libsql/LibsqlSetupHost.cs
@@ -14,7 +14,7 @@ namespace SirPixAlot.Core.EventStore.Libsql
         public async override Task StartAsync(CancellationToken cancellationToken)
         {
             await databaseClient.Execute("CREATE TABLE IF NOT EXISTS `canvas_grains` (`grain_id` TEXT NOT NULL, `version` INTEGER NOT NULL, `global_position` BIGINT NOT NULL, `event_type` TEXT NOT NULL, `data` TEXT NOT NULL, PRIMARY KEY ( `grain_id`, `version`))");
-
+            await databaseClient.Sync();
             await base.StartAsync(cancellationToken);
         }
 
